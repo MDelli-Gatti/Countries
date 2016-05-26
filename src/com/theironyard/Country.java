49b64107d3289ddf36764c8bf4f2205736c29aba
michaeldelli-gatti <michaelddg@gmail.com>
2016-05-26 12:21:53 -0400

    Initial commit

---
+++ src/com/theironyard/Country.java
@@ -0,0 +1,30 @@
+package com.theironyard;
+
+/**
+ * Created by michaeldelli-gatti on 5/26/16.
+ */
+public class Country {
+    String name;
+    String abbreviation;
+
+    public Country(String name, String abbreviation) {
+        this.name = name;
+        this.abbreviation = abbreviation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+}
